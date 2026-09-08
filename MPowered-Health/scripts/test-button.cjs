cc9183d75ddee81cc4cc089ab2aa62b43bd2d073
Sher-Wen <sherwenc@student.unimelb.edu.au>
2026-09-08 15:09:12 +1000

    added blank file to add button navigation tests

---
+++ MPowered-Health/scripts/test-button.cjs
@@ -0,0 +1,13 @@
+// tests that buttons are clickable and lead to correct page
+
+// login buttons
+
+// homescreen (pain profile) buttons
+
+// assessement buttons
+
+// myHealth buttons
+
+// care planner buttons
+
+// settings buttons
