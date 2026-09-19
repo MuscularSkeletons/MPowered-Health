0090a82c31edce28c5d9ffb2163aca5be90832ba
stuti <ssgupta@student.unimelb.edu.au>
2026-09-19 18:16:39 +1000

    added navigation tests to splash screen

--- MPowered-Health/__test__/splashscreen.test.tsx
+++ MPowered-Health/__test__/splashscreen.test.tsx
@@ -1,5 +1,19 @@
+jest.mock("@/context/authcontext", () => ({
+    useAuth: () => ({
+        user: null,
+        signIn: jest.fn(),
+        signUp: jest.fn(),
+        signOut: jest.fn(),
+        updateUser: jest.fn(),
+        isLoading: false,
+    }),
+}))
+
 import SplashScreen from '../src/app/(auth)/splashscreen';
-import { render, screen } from '@testing-library/react-native';
+import Login from '../src/app/(auth)/login';
+import Signup from '../src/app/(auth)/signup';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import {renderRouter} from 'expo-router/testing-library';
 
 //check if screen components render properly
 describe('splash screen components render properly', () => {
@@ -21,8 +35,37 @@ describe('splash screen components render properly', () => {
     /*
     test('rendering app info slide show', async() => {
 
-        //use fireEvent() and mocks
+        //maybe use fireEvent() and mocks
 
     })*/
 
 });
+
+describe ("Navigation to the correct screen", () => {
+    test('navigate to signup page when get started button is pressed', async() => {
+
+        await renderRouter( {
+            "/(auth)/signup" : Signup,
+            "/(auth)/splashscreen": SplashScreen,
+        }, {
+            initialUrl: "/(auth)/splashscreen",
+        });
+        
+        await fireEvent.press(screen.getByText("Get Started"));
+        expect(await screen.findByText('SIGN UP')).toBeOnTheScreen();
+    })
+
+    test('navigate to login page when sign in button is pressed', async() => {
+
+        await renderRouter( {
+            "/(auth)/login" : Login,
+            "/(auth)/splashscreen": SplashScreen,
+        }, {
+            initialUrl: "/(auth)/splashscreen",
+        });
+        
+        await fireEvent.press(screen.getByText("Sign in"));
+        expect(await screen.findByText('LOGIN')).toBeOnTheScreen();
+
+    })
+})
