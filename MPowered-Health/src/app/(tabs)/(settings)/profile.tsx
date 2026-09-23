e4f229c4debe072c8deb385326630a920e35cda1
Sher-Wen <sherwenc@student.unimelb.edu.au>
2026-09-23 23:04:52 +1000

    profilerated editable profile with currently unfunctional changeable options

--- MPowered-Health/src/app/(tabs)/(settings)/profile.tsx
+++ MPowered-Health/src/app/(tabs)/(settings)/profile.tsx
@@ -1,5 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { 
+  Text, 
+  View, 
+  StyleSheet,
+  TouchableOpacity, 
+} from "react-native";
 import { useRouter } from "expo-router";
+import { useAuth } from '@/context/authcontext';
+import { Ionicons } from "@expo/vector-icons";
+import { palette } from "@/constants/profile/ui";
+
 // temporary UI reusing auth ui
 import {
   AuthInput,
@@ -12,6 +21,8 @@ import {
 
 export default function ManageProfile() {
   const router = useRouter();
+  const { user, updateUser } = useAuth();
+
   return (
     <AuthScreen>
       <Text onPress={() => router.back()} style={authStyles.back}>‹ Back</Text>
@@ -20,9 +31,63 @@ export default function ManageProfile() {
         title="Edit your profile"
         description="Edit your information or update your security details"
       />
-      <Text>Edit Name</Text>
-      <Text>Edit Diagnosis</Text>
-      <Text>Edit Conditions</Text>
+
+      <TouchableOpacity style={styles.settingItem}>
+        <Text style={styles.settingLabel}>{user?.name || "No Name"}</Text>
+        <Ionicons 
+            name={"create-outline"}
+            style={styles.settingValue}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.settingItem}>
+        <Text style={styles.settingLabel}>{user?.formalDiagnosis || "No diagnosis given"}</Text>
+        <Ionicons 
+            name={"create-outline"}
+            style={styles.settingValue}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.settingItem}>
+        <Text style={styles.settingLabel}>Edit Conditions</Text>
+        <Ionicons 
+            name={"create-outline"}
+            style={styles.settingValue}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.settingItem}>
+        <Text style={styles.settingLabel}>{user?.email || "No email"}</Text>
+        <Ionicons 
+            name={"create-outline"}
+            style={styles.settingValue}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.settingItem}>
+        <Text style={styles.settingLabel}>Change password</Text>
+        <Ionicons 
+            name={"create-outline"}
+            style={styles.settingValue}
+        />
+      </TouchableOpacity>
     </AuthScreen>
   );
 }
+
+const styles = StyleSheet.create({
+  settingItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    backgroundColor: palette.surface,
+    borderRadius: 12,
+    marginBottom: 8,
+  },
+  settingLabel: {
+    fontSize: 20,
+    color: palette.success,
+  },
+  settingValue: {
+    fontSize: 25,
+    color: palette.success,
+  },
+})
